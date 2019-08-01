f39d560335d6df99a89d459fa27776e6fe279041
wluijk <wilfredluijk@gmail.com>
2019-08-01 09:43:59 +0200

    Added messages and new room structure

--- src/main/java/nl/snowmanxl/clickbattle/room/internal/RoomManagerImpl.java
+++ src/main/java/nl/snowmanxl/clickbattle/room/internal/RoomManagerImpl.java
@@ -1,10 +1,17 @@
 package nl.snowmanxl.clickbattle.room.internal;
 
+import nl.snowmanxl.clickbattle.messages.socket.MessageDispatcher;
 import nl.snowmanxl.clickbattle.messages.socket.MessageListenerManager;
+import nl.snowmanxl.clickbattle.messages.socket.RoomUpdateMessage;
 import nl.snowmanxl.clickbattle.model.SimpleParticipant;
-import nl.snowmanxl.clickbattle.room.*;
+import nl.snowmanxl.clickbattle.room.Participant;
+import nl.snowmanxl.clickbattle.room.Room;
+import nl.snowmanxl.clickbattle.room.RoomData;
+import nl.snowmanxl.clickbattle.room.RoomIdService;
+import nl.snowmanxl.clickbattle.room.RoomManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -18,19 +25,23 @@ public class RoomManagerImpl implements RoomManager {
     private final Map<Integer, Room> rooms = new HashMap<>();
     private final List<Consumer<Room>> roomChangeListeners = new ArrayList<>();
     private final RoomIdService idService;
-    private final RoomFactory roomFactory;
+    private final ApplicationContext context;
     private final MessageListenerManager messageListenerManager;
+    private final MessageDispatcher messageDispatcher;
 
-    public RoomManagerImpl(RoomIdService idService, RoomFactory roomFactory, MessageListenerManager messageListenerManager) {
+    public RoomManagerImpl(RoomIdService idService, ApplicationContext context, MessageListenerManager messageListenerManager,
+                           MessageDispatcher messageDispatcher) {
         this.idService = idService;
-        this.roomFactory = roomFactory;
+        this.context = context;
         this.messageListenerManager = messageListenerManager;
+        this.messageDispatcher = messageDispatcher;
     }
 
     @Override
     public int createRoom(RoomConfig config) {
         var id = idService.getNewRoomId();
-        var room = roomFactory.getNewRoomFor(config);
+        var room = context.getBean(Room.class);
+        room.configureRoom(id, config);
         rooms.put(id, room);
         LOGGER.trace("GameRoomImpl added {}", room);
         broadCastChange(room);
@@ -76,8 +87,8 @@ public class RoomManagerImpl implements RoomManager {
     }
 
     private void broadCastChange(Room room) {
-        LOGGER.trace("Broadcast room: {}", room);
-        roomChangeListeners.forEach(listener -> listener.accept(room));
+        LOGGER.trace("Dispatch room update: {}", room);
+        messageDispatcher.dispatchToRoom(room.getId(), new RoomUpdateMessage(RoomData.of(room)));
     }
 
 }
